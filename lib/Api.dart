ff52cb22f1c2171f4845b6575fbcab9953594c88
Felipe Gameiro <felipegameiro@gmail.com>
2019-12-15 21:43:14 -0300

    Create API call to fetch images

--- lib/Api.dart
+++ lib/Api.dart
@@ -1,19 +1,32 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'BreedsModel.dart';
-
-const baseURL = "https://dog.ceo/api/breeds/list/all";
+import 'Models.dart';
 
 class API {
-  static Future<Breeds> getAllBreedsAlternative() async {
-    final response = await http.get(baseURL);
+  static Future<Breeds> getAllBreeds() async {
+    final String allBreedsURL = "https://dog.ceo/api/breeds/list/all";
+
+    final response = await http.get(allBreedsURL);
 
     if (response.statusCode == 200) {
       var result = Breeds.fromJson(json.decode(response.body));
       return result;
     } else {
-      throw Exception('Failed to load post');
+      throw Exception('Failed to load breeds');
+    }
+  }
+
+  static Future<DogImages> getImageURLs(String breed) async {
+    final String imagesURLs = "https://dog.ceo/api/breed/$breed/images";
+
+    final response = await http.get(imagesURLs);
+
+    if (response.statusCode == 200) {
+      var result = DogImages.fromJson(json.decode(response.body));
+      return result;
+    } else {
+      throw Exception('Failed to load urls');
     }
   }
 }
